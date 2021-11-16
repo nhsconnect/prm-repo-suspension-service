b7d27661948dce9826d662cebe36e6963fc94b31
Jack Slater <jack.slater1@nhs.net>
2021-11-16 14:21:05 +0000

    [PRMT-2285] initial commit

---
+++ src/main/java/uk/nhs/prm/repo/suspension/service/PrmRepoSuspensionServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.repo.suspension.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrmRepoSuspensionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrmRepoSuspensionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/nhs/prm/repo/suspension/service/PrmRepoSuspensionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.repo.suspension.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrmRepoSuspensionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
