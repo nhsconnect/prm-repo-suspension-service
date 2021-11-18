a32fd45acedff2abfb318ac206ab2fc8396461aa
Jack Slater <jack.slater1@nhs.net>
2021-11-18 09:28:31 +0000

    [PRMT-2285] enable scheduling at the application level

--- src/main/java/uk/nhs/prm/repo/suspension/service/PrmRepoSuspensionServiceApplication.java
+++ src/main/java/uk/nhs/prm/repo/suspension/service/PrmRepoSuspensionServiceApplication.java
@@ -2,8 +2,10 @@ package uk.nhs.prm.repo.suspension.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PrmRepoSuspensionServiceApplication {
 
     public static void main(String[] args) {
