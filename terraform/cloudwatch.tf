a687a221abe0fe4cf1ee40bda7348771da3da083
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-25 16:14:03 +0000

    [PRMT-2313] Updated error log alarm to treat missing data as not breaching

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -51,7 +51,7 @@ resource "aws_cloudwatch_metric_alarm" "error_log_alarm" {
   namespace                 = local.suspension_service_metric_namespace
   statistic                 = "Sum"
   alarm_description         = "This alarm monitors errors logs in ${var.component_name}"
-  treat_missing_data        = "missing"
+  treat_missing_data        = "notBreaching"
   actions_enabled           = "true"
   alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
 }
