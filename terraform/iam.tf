43f3ff09ee02466feb0fb4743b859f91d9e49809
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-11-25 17:39:26 +0300

    [PRMT-2411] Added sns failure feedback role.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -174,3 +174,52 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_metrics_policy_attach" {
   role       = aws_iam_role.component-ecs-role.name
   policy_arn = aws_iam_policy.cloudwatch_metrics_policy.arn
 }
+
+resource "aws_iam_role" "sns_failure_feedback_role" {
+  name               = "${var.environment}-${var.component_name}-sns-failure-feedback-role"
+  assume_role_policy = data.aws_iam_policy_document.sns_service_assume_role_policy.json
+  description        = "Allows logging of SNS delivery failures in ${var.component_name}"
+
+  tags = {
+    Environment = var.environment
+    CreatedBy   = var.repo_name
+  }
+}
+
+data "aws_iam_policy_document" "sns_failure_feedback_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+      "logs:PutRetentionPolicy"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "sns_failure_feedback_policy" {
+  name   = "${var.environment}-${var.component_name}-sns-failure-feedback"
+  policy = data.aws_iam_policy_document.sns_failure_feedback_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "sns_failure_feedback_policy_attachment" {
+  role       = aws_iam_role.sns_failure_feedback_role.name
+  policy_arn = aws_iam_policy.sns_failure_feedback_policy.arn
+}
+
+data "aws_iam_policy_document" "sns_service_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "sns.amazonaws.com"
+      ]
+    }
+  }
+}
