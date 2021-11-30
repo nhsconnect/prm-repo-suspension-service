bdcf14675d4ad8c0b79298ba1568ad21700b128e
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-11-30 17:01:49 +0300

    [PRMT-2411] Added schedule config to publish metrics.

---
+++ src/main/java/uk/nhs/prm/repo/suspension/service/config/ScheduledConfig.java
@@ -0,0 +1,12 @@
+package uk.nhs.prm.repo.suspension.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@Configuration
+@EnableScheduling
+@ComponentScan("uk.nhs.prm.repo.suspension.service.metrics")
+public class ScheduledConfig {
+}
