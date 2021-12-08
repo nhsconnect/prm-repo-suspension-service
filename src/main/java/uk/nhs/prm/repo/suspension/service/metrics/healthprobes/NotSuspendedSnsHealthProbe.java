e9b477350c7565ed38abb9ccc2e17c010fae013a
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-12-08 12:58:53 +0000

    [PRMT-2470] Used snsClient bean instead of creating new instance

--- src/main/java/uk/nhs/prm/repo/suspension/service/metrics/healthprobes/NotSuspendedSnsHealthProbe.java
+++ src/main/java/uk/nhs/prm/repo/suspension/service/metrics/healthprobes/NotSuspendedSnsHealthProbe.java
@@ -1,6 +1,7 @@
 package uk.nhs.prm.repo.suspension.service.metrics.healthprobes;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.GetTopicAttributesRequest;
@@ -10,15 +11,17 @@ import uk.nhs.prm.repo.suspension.service.metrics.AppConfig;
 @Component
 public class NotSuspendedSnsHealthProbe implements HealthProbe {
     private final AppConfig config;
+    private final SnsClient snsClient;
 
-    public NotSuspendedSnsHealthProbe(AppConfig config) {
+    @Autowired
+    public NotSuspendedSnsHealthProbe(AppConfig config, SnsClient snsClient) {
         this.config = config;
+        this.snsClient = snsClient;
     }
 
     @Override
     public boolean isHealthy() {
         try {
-            var snsClient = SnsClient.create();
             snsClient.getTopicAttributes(GetTopicAttributesRequest.builder().topicArn(config.notSuspendedSnsTopicArn()).build());
             return true;
         } catch (RuntimeException exception) {
