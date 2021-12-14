c24b407ccdd60579ac894f35b7e7b8fa34df8e32
Emily Youngs <emily.youngs1@nhs.net>
2021-12-14 13:34:45 +0000

    [PRMT-2292] Create the ‘MOF updated’ SNS topic and queue

--- terraform/encyrption-key.tf
+++ terraform/encyrption-key.tf
@@ -58,3 +58,64 @@ data "aws_iam_policy_document" "not_suspended_kms_key_policy_doc" {
     resources = ["*"]
   }
 }
+
+resource "aws_kms_key" "mof_updated" {
+  description = "Custom KMS Key to enable server side encryption for SNS and SQS"
+  policy      = data.aws_iam_policy_document.mof_updated_kms_key_policy_doc.json
+
+  tags = {
+    Name        = "${var.environment}-mof-updated-queue-encryption-kms-key"
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_kms_alias" "mof_updated_encryption" {
+  name          = "alias/mof-updated-encryption-kms-key"
+  target_key_id = aws_kms_key.mof_updated.id
+}
+
+data "aws_iam_policy_document" "mof_updated_kms_key_policy_doc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type        = "AWS"
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
+  }
+}
--- terraform/queue.tf
+++ terraform/queue.tf
@@ -1,6 +1,7 @@
 locals {
   suspensions_queue_name = "${var.environment}-${var.component_name}-suspensions-queue"
   not_suspended_queue_name = "${var.environment}-${var.component_name}-not-suspended-observability-queue"
+  mof_updated_queue_name = "${var.environment}-${var.component_name}-mof-updated-queue"
 }
 
 resource "aws_sqs_queue" "suspensions" {
@@ -133,3 +134,73 @@ data "aws_iam_policy_document" "suspensions_sns_topic_access_to_queue" {
     }
   }
 }
+
+resource "aws_sns_topic" "mof_updated" {
+  name = "${var.environment}-${var.component_name}-mof-updated-sns-topic"
+  kms_master_key_id = aws_kms_key.mof_updated.id
+  sqs_failure_feedback_role_arn = aws_iam_role.sns_failure_feedback_role.arn
+
+  tags = {
+    Name = "${var.environment}-${var.component_name}-mof-updated-sns-topic"
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_sqs_queue" "mof_updated" {
+  name                       = local.mof_updated_queue_name
+  message_retention_seconds  = 1800
+  kms_master_key_id = aws_kms_key.mof_updated.id
+
+  tags = {
+    Name = local.mof_updated_queue_name
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_ssm_parameter" "mof_updated_queue" {
+  name  = "/repo/${var.environment}/output/${var.component_name}/mof-updated-queue-name"
+  type  = "String"
+  value = aws_sqs_queue.mof_updated.name
+}
+
+
+resource "aws_sns_topic_subscription" "mof_updated" {
+  protocol             = "sqs"
+  raw_message_delivery = true
+  topic_arn            = aws_sns_topic.mof_updated.arn
+  endpoint             = aws_sqs_queue.mof_updated.arn
+}
+
+resource "aws_sqs_queue_policy" "mof_updated_events_subscription" {
+  queue_url = aws_sqs_queue.mof_updated.id
+  policy    = data.aws_iam_policy_document.mof_updated_events_policy_doc.json
+}
+
+data "aws_iam_policy_document" "mof_updated_events_policy_doc" {
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    resources = [
+      aws_sqs_queue.mof_updated.arn
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_sns_topic.mof_updated.arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}
+
