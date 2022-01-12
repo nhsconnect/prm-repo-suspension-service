0e4fdde29f25781dc8acb011884542d97fc73cc5
eyupaksu-nhs <eyup.aksu@nhs.net>
2022-01-12 17:10:17 +0000

    [PRMT-2293] Added update pds service test case.

--- src/test/java/uk/nhs/prm/repo/suspension/service/pds/PdsServiceTest.java
+++ src/test/java/uk/nhs/prm/repo/suspension/service/pds/PdsServiceTest.java
@@ -4,19 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.client.RestTemplate;
-import uk.nhs.prm.repo.suspension.service.config.Tracer;
 import uk.nhs.prm.repo.suspension.service.http.HttpServiceClient;
 import uk.nhs.prm.repo.suspension.service.model.PdsAdaptorSuspensionStatusResponse;
-
-import java.util.HashMap;
-import java.util.Map;
+import uk.nhs.prm.repo.suspension.service.model.UpdateManagingOrganisationRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
@@ -44,10 +37,26 @@ class PdsServiceTest {
         String expectedUrl = "http://pds-adaptor/suspended-patient-status/1234567890";
         var parsedStatus = aStatus();
 
-        when(client.get(expectedUrl, "suspension-service","PASS")).thenReturn("some status");
+        when(client.get(expectedUrl, "suspension-service", "PASS")).thenReturn("some status");
         when(responseParser.parse("some status")).thenReturn(parsedStatus);
 
         var status = pdsService.isSuspended("1234567890");
+        assertThat(status).isEqualTo(parsedStatus);
+    }
+
+    @Test
+    public void shouldUpdatePatientStatusFromPdsServiceUrlAndParseResponse() {
+        setField(pdsService, "serviceUrl", "http://pds-adaptor"); // @todo fertling :/
+        setField(pdsService, "suspensionServicePassword", "PASS");
+
+        String expectedUrl = "http://pds-adaptor/suspended-patient-status/1234567890";
+        var parsedStatus = aStatus();
+        var requestPayload = new UpdateManagingOrganisationRequest("hello", "bob");
+
+        when(client.put(expectedUrl, "suspension-service", "PASS", requestPayload)).thenReturn("some status");
+        when(responseParser.parse("some status")).thenReturn(parsedStatus);
+
+        var status = pdsService.updateMof("1234567890", "hello", "bob");
         assertThat(status).isEqualTo(parsedStatus);
     }
 
