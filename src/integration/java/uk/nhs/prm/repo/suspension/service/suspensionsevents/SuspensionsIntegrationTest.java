f79da27a81d24030d2f01f14e49a9de2bf39ce67
Dan Moore <dan.moore1@nhs.net>
2022-01-12 15:43:19 +0000

    [PRMT-2293] Fix integration test application context

--- src/integration/java/uk/nhs/prm/repo/suspension/service/suspensionsevents/SuspensionsIntegrationTest.java
+++ src/integration/java/uk/nhs/prm/repo/suspension/service/suspensionsevents/SuspensionsIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -25,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest()
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = LocalStackAwsConfig.class)
+@ContextConfiguration(classes = {LocalStackAwsConfig.class, RestTemplate.class})
 @EnableScheduling
 public class SuspensionsIntegrationTest {
 
