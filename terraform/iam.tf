35b11c90865cdfbeeef8582b20373f2f083d7429
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-05-11 14:48:48 +0100

    [PRMT-2554] Added queue policy for repo-incoming-observability to be able to send something to queue

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -533,3 +533,33 @@ resource "aws_iam_role_policy_attachment" "ecs_dynamo_attach" {
   role       = aws_iam_role.component-ecs-role.name
   policy_arn = aws_iam_policy.dynamodb-table-access.arn
 }
+
+resource "aws_sqs_queue_policy" "repo_incoming_observability" {
+  queue_url = aws_sqs_queue.repo_incoming_observability_queue.id
+  policy    = data.aws_iam_policy_document.repo_incoming_observability_topic_access_to_queue.json
+}
+
+data "aws_iam_policy_document" "repo_incoming_observability_topic_access_to_queue" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    resources = [
+      aws_sqs_queue.repo_incoming_observability_queue.arn
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      values   = [aws_sns_topic.repo_incoming.arn]
+      variable = "aws:SourceArn"
+    }
+  }
+}
