5e400d78a5af51e531b471528ef25c8d1daf2e35
Mario Giampietri <mario.giampietri1@nhs.net>
2022-07-08 13:45:14 +0100

    [PRMT-2561] configured scale up/down to be up for suspension service only

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,9 +2,9 @@ locals {
   error_logs_metric_name              = "ErrorCountInLogs"
   suspension_service_metric_namespace = "SuspensionService"
   not_suspended_sns_topic_name        = "${var.environment}-suspension-service-not-suspended-sns-topic"
-  sns_topic_namespace = "AWS/SNS"
-  sqs_namespace = "AWS/SQS"
-  sns_topic_error_logs_metric_name = "NumberOfNotificationsFailed"
+  sns_topic_namespace                 = "AWS/SNS"
+  sqs_namespace                       = "AWS/SQS"
+  sns_topic_error_logs_metric_name    = "NumberOfNotificationsFailed"
 }
 
 resource "aws_cloudwatch_log_group" "log_group" {
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_log_group" "log_group" {
 
   tags = {
     Environment = var.environment
-    CreatedBy= var.repo_name
+    CreatedBy   = var.repo_name
   }
 }
 
@@ -30,158 +30,159 @@ resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "error_log_alarm" {
-  alarm_name                = "${var.environment}-${var.component_name}-error-logs"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "0"
-  evaluation_periods        = "1"
-  period                    = "60"
-  metric_name               = local.error_logs_metric_name
-  namespace                 = local.suspension_service_metric_namespace
-  statistic                 = "Sum"
-  alarm_description         = "This alarm monitors errors logs in ${var.component_name}"
-  treat_missing_data        = "notBreaching"
-  actions_enabled           = "true"
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_name          = "${var.environment}-${var.component_name}-error-logs"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "0"
+  evaluation_periods  = "1"
+  period              = "60"
+  metric_name         = local.error_logs_metric_name
+  namespace           = local.suspension_service_metric_namespace
+  statistic           = "Sum"
+  alarm_description   = "This alarm monitors errors logs in ${var.component_name}"
+  treat_missing_data  = "notBreaching"
+  actions_enabled     = "true"
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "not_suspended_sns_topic_error_log_alarm" {
-  alarm_name                = "${local.not_suspended_sns_topic_name}-error-logs"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "0"
-  evaluation_periods        = "1"
-  period                    = "60"
-  metric_name               = local.sns_topic_error_logs_metric_name
-  namespace                 = local.sns_topic_namespace
-  dimensions = {
+  alarm_name          = "${local.not_suspended_sns_topic_name}-error-logs"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "0"
+  evaluation_periods  = "1"
+  period              = "60"
+  metric_name         = local.sns_topic_error_logs_metric_name
+  namespace           = local.sns_topic_namespace
+  dimensions          = {
     TopicName = local.not_suspended_sns_topic_name
   }
-  statistic                 = "Sum"
-  alarm_description         = "This alarm monitors errors logs in ${local.not_suspended_sns_topic_name}"
-  treat_missing_data        = "notBreaching"
-  actions_enabled           = "true"
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  statistic           = "Sum"
+  alarm_description   = "This alarm monitors errors logs in ${local.not_suspended_sns_topic_name}"
+  treat_missing_data  = "notBreaching"
+  actions_enabled     = "true"
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_out_of_order_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-out-of-order-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the out of order audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-out-of-order-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the out of order audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.event_out_of_order_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_not_suspended_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-not-suspended-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the not suspended audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-not-suspended-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the not suspended audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.not_suspended_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_mof_not_updated_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-mof-not-updated-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the MOF not updated audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-mof-not-updated-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the MOF not updated audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.mof_not_updated_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_mof_updated_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-mof-updated-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the MOF updated audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-mof-updated-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the MOF updated audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.mof_updated_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_deceased_patient_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-deceased-patient-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the deceased patient audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-deceased-patient-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the deceased patient audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.deceased_patient_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_invalid_suspension_dlq_audit" {
-  alarm_name                = "${var.environment}-${var.component_name}-invalid-suspension-dlq-audit"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 = "900"
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "This alarm triggers when messages on the invalid suspensions dlq audit queue is not polled by splunk in last 15 mins"
-  statistic                 = "Maximum"
-  period                    = "900"
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-invalid-suspension-dlq-audit"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "900"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "This alarm triggers when messages on the invalid suspensions dlq audit queue is not polled by splunk in last 15 mins"
+  statistic           = "Maximum"
+  period              = "900"
+  dimensions          = {
     QueueName = aws_sqs_queue.invalid_suspension_audit.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspensions_queue_age_of_message" {
-  alarm_name                = "${var.environment}-${var.component_name}-queue-age-of-message"
-  comparison_operator       = "GreaterThanThreshold"
-  threshold                 =  var.threshold_for_suspensions_queue_age_of_message
-  evaluation_periods        = "1"
-  metric_name               = "ApproximateAgeOfOldestMessage"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "Alarm to alert approximate time for message in the queue"
-  statistic                 = "Maximum"
-  period                    = var.period_of_age_of_message_metric
-  dimensions = {
+  alarm_name          = "${var.environment}-${var.component_name}-queue-age-of-message"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = var.threshold_for_suspensions_queue_age_of_message
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = local.sqs_namespace
+  alarm_description   = "Alarm to alert approximate time for message in the queue"
+  statistic           = "Maximum"
+  period              = var.period_of_age_of_message_metric
+  dimensions          = {
     QueueName = aws_sqs_queue.suspensions.name
   }
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+  alarm_actions       = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_service_scale_up_alarm" {
-  alarm_name                = "${var.environment}-suspensions-service-scale-up"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  threshold                 = "10"
-  alarm_description         = "Scale up alarm for suspension-service"
-  actions_enabled           = true
-  alarm_actions             = [aws_appautoscaling_policy.scale_up.arn]
+  count               = var.is_end_of_transfer_service ? 0 : 1
+  alarm_name          = "${var.environment}-${var.component_name}-scale-up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  threshold           = "10"
+  alarm_description   = "Scale up alarm for suspension-service"
+  actions_enabled     = true
+  alarm_actions       = [aws_appautoscaling_policy.scale_up.arn]
 
   metric_query {
     id          = "e1"
@@ -208,19 +209,20 @@ resource "aws_cloudwatch_metric_alarm" "suspension_service_scale_up_alarm" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "suspension_service_scale_down_alarm" {
-  alarm_name                = "${var.environment}-${var.component_name}-scale-down"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  threshold                 =  var.scale_down_number_of_empty_receives_count * var.core_task_number
-  evaluation_periods        = "1"
-  metric_name               = "NumberOfEmptyReceives"
-  namespace                 = local.sqs_namespace
-  alarm_description         = "Alarm to alert when all events are processed in the queue"
-  statistic                 = "Sum"
-  period                    = 300
-  dimensions = {
+  count               = var.is_end_of_transfer_service ? 0 : 1
+  alarm_name          = "${var.environment}-${var.component_name}-scale-down"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = var.scale_down_number_of_empty_receives_count * var.core_task_number
+  evaluation_periods  = "1"
+  metric_name         = "NumberOfEmptyReceives"
+  namespace           = local.sqs_namespace
+  alarm_description   = "Alarm to alert when all events are processed in the queue"
+  statistic           = "Sum"
+  period              = 300
+  dimensions          = {
     QueueName = aws_sqs_queue.suspensions.name
   }
-  treat_missing_data        = "notBreaching"
-  actions_enabled           = var.enable_scale_action
-  alarm_actions             = [aws_appautoscaling_policy.scale_down.arn]
+  treat_missing_data  = "notBreaching"
+  actions_enabled     = var.enable_scale_action
+  alarm_actions       = [aws_appautoscaling_policy.scale_down.arn]
 }
