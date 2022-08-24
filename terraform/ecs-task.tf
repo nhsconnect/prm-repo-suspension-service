0b4d496d458ec795262f6f1e0b6cef9709342802
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-08-24 11:51:01 +0100

    [PRMT-2020] Removed default '-' for safe listed ods codes.
    Co-authored-by: romanenhs <romane.castera1@nhs.net>

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -29,7 +29,7 @@ locals {
     { name = "DYNAMODB_TABLE_NAME", value = aws_dynamodb_table.suspensions.name },
     { name = "PDS_ADAPTOR_URL", value = "https://pds-adaptor.${var.environment_dns_zone}.patient-deductions.nhs.uk" },
     { name = "REPO_ODS_CODE", value = data.aws_ssm_parameter.repo_ods_code.value },
-    { name = "SAFE_LISTED_ODS_CODES", valueFrom = data.aws_ssm_parameter.safe_listed_ods_codes.value},
+    { name = "SAFE_LISTED_ODS_CODES", value = data.aws_ssm_parameter.safe_listed_ods_codes.value},
     { name = "PROCESS_ONLY_SAFE_LISTED_ODS_CODES", value = tostring(var.process_only_safe_listed_ods_codes) }
   ]
   secrets = [
