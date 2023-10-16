f0c47aedcb4f9482515497bec67ffede164a284a
GaryBrims <gary.brims1@nhs.net>
2023-10-16 16:57:37 +0100

    PRM4134 - Added Ingestion Bucket Policy

--- terraform/modules/suspension-service/s3.tf
+++ terraform/modules/suspension-service/s3.tf
@@ -4,9 +4,38 @@ locals {
 
 resource "aws_s3_bucket" "ingestion_bucket" {
   bucket = local.ingestion_bucket_name
-  acl           = "private"
-  tags = {
-    Name        = local.ingestion_bucket_name
-    Environment = var.environment
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+    tags = {
+      Name        = local.ingestion_bucket_name
+      Environment = var.environment
+    }
   }
 }
+
+  resource "aws_s3_bucket_policy" "ingestion_bucket_policy" {
+    bucket = aws_s3_bucket.ingestion_bucket.id
+    policy = jsonencode({
+      "Statement" : [
+        {
+          Effect : "Deny",
+          Principal : "*",
+          Action : "s3:*",
+          Resource : "${aws_s3_bucket.ingestion_bucket.arn}/*",
+          Condition : {
+            Bool : {
+              "aws:SecureTransport" : "false"
+            }
+          }
+        }
+      ]
+    }
+    )
+  }
+
+
