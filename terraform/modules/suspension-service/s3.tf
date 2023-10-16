708a1446c04f1158cd1d53a1f55c48f680743d05
GaryBrims <gary.brims1@nhs.net>
2023-10-16 15:53:46 +0100

    PRM413 - try deprecated ACL command

--- terraform/modules/suspension-service/s3.tf
+++ terraform/modules/suspension-service/s3.tf
@@ -4,33 +4,9 @@ locals {
 
 resource "aws_s3_bucket" "ingestion_bucket" {
   bucket = local.ingestion_bucket_name
-
+  acl           = "private"
   tags = {
     Name        = local.ingestion_bucket_name
     Environment = var.environment
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "ingestion_bucket" {
-  bucket = aws_s3_bucket.ingestion_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-
-resource "aws_s3_bucket_acl" ingestion_bucket {
-  depends_on = [aws_s3_bucket_ownership_controls.ingestion_bucket]
-  bucket     = aws_s3_bucket.ingestion_bucket.id
-  acl        = "private"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "ingestion_bucket" {
-  bucket = aws_s3_bucket.ingestion_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-    }
   }
 }
