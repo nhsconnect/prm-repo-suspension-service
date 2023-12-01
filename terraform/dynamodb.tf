6c4ffe498259329fcf6418a44ce2b927af287c6e
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2023-12-01 14:24:25 +0000

    [PRMT-4252-DYNAMODB] Added `deletion_protection_enabled = true`, verified `point_in_time_recovery` is enabled and run `terraform fmt`. (#84)

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,19 +1,20 @@
 resource "aws_dynamodb_table" "suspensions" {
-    name = "${var.environment}-${var.component_name}-dynamodb"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "nhs_number"
+  name                        = "${var.environment}-${var.component_name}-dynamodb"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "nhs_number"
+  deletion_protection_enabled = true
 
-    server_side_encryption {
-        enabled =  true
-        kms_key_arn = aws_kms_key.suspension_dynamodb_kms_key.arn
-    }
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.suspension_dynamodb_kms_key.arn
+  }
 
-    point_in_time_recovery {
-        enabled =  true
-    }
+  point_in_time_recovery {
+    enabled = true
+  }
 
-    attribute {
-        name = "nhs_number"
-        type = "S"
-    }
+  attribute {
+    name = "nhs_number"
+    type = "S"
+  }
 }
