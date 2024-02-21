0aa08479b17271db0c40abad8994b69630269197
chrisbloe-nhse <151756494+chrisbloe-nhse@users.noreply.github.com>
2024-02-21 13:07:21 +0000

    Upping AWS povider version (#160)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,36 @@
 provider "aws" {
-  profile   = "default"
-  region    = var.region
+  profile = "default"
+  region  = var.region
 }
 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.44.0"
+      version = "3.76.1"
     }
   }
 }
 
 module "end-of-transfer-service" {
-  count = var.is_end_of_transfer_service ? 1 : 0
-  source    = "./modules/end-of-transfer-service/"
-  environment    = var.environment
+  count       = var.is_end_of_transfer_service ? 1 : 0
+  source      = "./modules/end-of-transfer-service/"
+  environment = var.environment
 
-  component_name = var.component_name
-  metric_namespace = var.metric_namespace
-  repo_name      = var.repo_name
+  component_name    = var.component_name
+  metric_namespace  = var.metric_namespace
+  repo_name         = var.repo_name
   ecs_desired_count = var.ecs_desired_count
 }
 
 module "suspension-service" {
-  count = var.is_end_of_transfer_service ? 0 : 1
-  source    = "./modules/suspension-service/"
-  environment    = var.environment
-  component_name = var.component_name
-  metric_namespace = var.metric_namespace
-  repo_name      = var.repo_name
-  sns_sqs_role_arn = aws_iam_role.sns_failure_feedback_role.arn
+  count               = var.is_end_of_transfer_service ? 0 : 1
+  source              = "./modules/suspension-service/"
+  environment         = var.environment
+  component_name      = var.component_name
+  metric_namespace    = var.metric_namespace
+  repo_name           = var.repo_name
+  sns_sqs_role_arn    = aws_iam_role.sns_failure_feedback_role.arn
   ecs_cluster_name    = aws_ecs_cluster.ecs-cluster.name
   ecs_service_name    = aws_ecs_service.ecs-service.name
   scale_up_expression = var.scale_up_expression
